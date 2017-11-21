15130893ad5b99d0df40ee23c2bfc287df94b569
Christopher S. Case <chris.case@g33xnexus.com>
2017-11-21 16:53:09 -0600

    converted to knex session store, and changed 404 fallback.

--- server.js
+++ server.js
@@ -22,33 +22,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const RDBStore = require('express-session-rethinkdb')(session);
+const KnexSessionStore = require('connect-session-knex')(session);
 const passport = require('passport');
+
+const dbMan  = require('./server/database');
 
 // Auth
 const GoogleAuth = require('./server/auth/google');
 
 // Routes
 const accountsRoute = require('./server/routes/accounts');
-const wikiRoute = require('./server/routes/wiki');
+// const wikiRoute = require('./server/routes/wiki');
 const routeUtils = require('./server/routes/utils');
 
 //----------------------------------------------------------------------------------------------------------------------
 
-const rdbStore = new RDBStore({
-    connectOptions: config.rethink,
-    table: 'sessions',
-
-    // If you do not set cookie.maxAge in session middleware, sessions will last until the user closes their browser.
-    // However we cannot keep the session data infinitely (for size and security reasons). In this case, this setting
-    // defines the maximum length of a session, even if the user does not close their browser. (2 days)
-    sessionTimeout: 2 * 24 * 60 * 60 * 1000,
-
-    // RethinkDB does not yet provide an expiration function ( like SETEX for Redis ), so we have to remove the old
-    // expired sessions from the database intermittently. This is the time interval in milliseconds between flushing of
-    // expired sessions. (1 hour)
-    flushInterval: 60 * 60 * 1000,
-    debug: config.debug
+const store = new KnexSessionStore({
+    sidfieldname: config.key || 'sid',
+    knex: dbMan.getDB(),
+    createTable: true
 });
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -70,7 +62,7 @@ app.use(session({
     secret: config.secret || 'nosecret',
     key: config.key || 'sid',
     resave: false,
-    store: rdbStore,
+    store,
 
     // maxAge = 7 days
     cookie: { maxAge: 7 * 24 * 60 * 60 * 1000, secure: !config.debug },
@@ -100,8 +92,8 @@ if(config.overrideAuth)
 app.use(express.static(path.resolve('./dist')));
 
 // Set up our application routes
-app.use('/accounts', accountsRoute);
-app.use('/wiki', wikiRoute);
+app.use('/account', accountsRoute);
+// app.use('/wiki', wikiRoute);
 
 // Serve index.html for any html requests, but 404 everything else.
 app.get('*', (request, response) => {
@@ -109,7 +101,12 @@ app.get('*', (request, response) => {
         html: routeUtils.serveIndex,
         json: (request, response) =>
         {
-            response.status(404).end();
+            response.status(404)
+                .json({
+                    name: "NotFoundError",
+                    message: `Requested url '${ request.url }' not found`,
+                    code: "ERR_NOT_FOUND"
+                });
         }
     })
 });
