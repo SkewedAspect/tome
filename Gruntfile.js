c9bceeebe01c9f55f7beca95ac0b4fe7a7d1e5a1
Christopher S. Case <chris.case@g33xnexus.com>
2017-11-24 15:06:33 -0600

    switched to webpack.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 //----------------------------------------------------------------------------------------------------------------------
-// Tome Gruntfile.
+// Gruntfile
+//----------------------------------------------------------------------------------------------------------------------
+
+const webpackConfig = require('./webpack.config');
+
 //----------------------------------------------------------------------------------------------------------------------
 
 module.exports = function(grunt)
@@ -8,111 +12,42 @@ module.exports = function(grunt)
         clean: ['dist'],
         copy: {
             index: {
-                src: 'client/index.html',
-                dest: 'dist/index.html'
-            },
-            manifest: {
-                src: 'client/manifest.json',
-                dest: 'dist/manifest.json'
+                expand: true,
+                cwd: 'client',
+                src: ['index.html'],
+                dest: 'dist'
             },
             static: {
                 expand: true,
-                cwd: 'client',
-                src: 'static/**/*',
-                dest: 'dist'
+                cwd: 'client/static',
+                src: ['**/*.*'],
+                dest: 'dist/static'
             },
-            html: {
-                expand: true,
-                cwd: 'client',
-                src: '**/*.html',
-                dest: 'dist'
-            },
-            materialize: {
-                expand: true,
-                cwd: 'node_modules/vue-material/dist',
-                src: '*.css',
-                dest: 'dist/vendor/vue-material'
-            },
-            vendor: {
-                expand: true,
-                cwd: 'node_modules',
-                src: ['spinkit/**/*.css'],
-                dest: 'dist/vendor'
-            }
         },
-        browserify: {
+        webpack: {
+            build: webpackConfig
+        },
+        'webpack-dev-server': {
             options: {
-                browserifyOptions: {
-                    debug: true
-                },
-                transform: [
-                    ["vueify"],
-                    ["babelify"]
-                ]
+                ...webpackConfig.devServer
             },
-            debug: {
-                options: {
-                    watch: true,
-                    watchOptions: {
-                        ignoreWatch: true
-                    },
-                    browserifyOptions: {
-                        debug: true
-                    }
-                },
-                files: {
-                    "./dist/app.js": "client/app.js"
-                }
-            }
-        },
-        sass: {
-            dist: {
-                options: {
-                    includePaths: ['client/scss', 'client'],
-                    style: 'expanded'
-                },
-                files: {
-                    'dist/css/app.css': 'client/scss/theme.scss'
-                }
-            }
-        },
-        postcss: {
-            options: {
-                processors: [ require('autoprefixer')({browsers: 'last 2 versions'}) ]
-            },
-            dist: {
-                src: 'dist/css/*.css'
-            }
-        },
-        watch: {
-            index: {
-                files: ["client/index.html"],
-                tasks: ["copy:index"]
-            },
-            html: {
-                files: ["client/**/*.html"],
-                tasks: ["copy:html"]
-            },
-            scss: {
-                files: ["client/scss/**/*.scss"],
-                tasks: ["sass", "postcss"]
+            start: {
+                webpack: webpackConfig
             }
         }
     });
 
     //------------------------------------------------------------------------------------------------------------------
 
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-postcss');
-    grunt.loadNpmTasks("grunt-browserify");
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-webpack');
 
     //------------------------------------------------------------------------------------------------------------------
 
-    grunt.registerTask("build", ["clean", "sass", "postcss", "copy", "browserify"]);
-    grunt.registerTask("default", ["build", 'watch']);
+    grunt.registerTask("build", ["clean", "copy", "webpack:build"]);
+    grunt.registerTask("watch", ["clean", "copy", "webpack-dev-server:start"]);
+    grunt.registerTask("default", ["watch"]);
 
     //------------------------------------------------------------------------------------------------------------------
 };
