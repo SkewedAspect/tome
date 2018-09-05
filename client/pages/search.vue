d436b40a328174635578ff4491513136363d0a9a
Christopher S. Case <chris.case@g33xnexus.com>
2018-09-04 22:57:20 -0500

    search results working.

--- client/pages/search.vue
+++ client/pages/search.vue
@@ -11,14 +11,33 @@
             <h4 class="text-center">No results found.</h4>
         </div>
         <div v-else>
-            Results: {{ searchResults }}
+            <header>
+                <div class="float-right text-muted mt-2">
+                    <small>({{ searchResults.length }} results)</small>
+                </div>
+                <h4>Search Results:</h4>
+                <hr class="mt-0 mb-1">
+            </header>
+            <p class="text-center text-info mb-0 mt-0">
+                <small><i>Results are sorted from best to worst match.</i></small>
+            </p>
+            <ul class="fa-ul">
+                <li class="mt-4" v-for="results in searchResults" :key="results.page.id">
+                    <font-awesome-icon icon="file-alt" transform="down-3" list-item></font-awesome-icon>
+                    <router-link :to="'/wiki' + results.page.path"><h5 class="mb-0" v-html="results.match.title"></h5></router-link>
+                    <small class="text-muted d-block text-monospace">
+                        <span>/wiki{{ results.page.path }}</span>
+                    </small>
+                    <div v-html="results.match.body"></div>
+                </li>
+            </ul>
         </div>
     </b-container>
 </template>
 
 <!--------------------------------------------------------------------------------------------------------------------->
 
-<style lang="scss" scoped>
+<style lang="scss">
     #search-page {
     }
 </style>
